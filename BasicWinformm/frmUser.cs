3c5d5eb0bda9f4d6cd924ce1add037cca1d417a8
tdnphuc <18t1021242@husc.edu.vn>
2021-09-27 07:55:06 +0700

    User

--- BasicWinformm/frmUser.cs
+++ BasicWinformm/frmUser.cs
@@ -1,40 +1,12 @@
-﻿using BasicWinformm.Entities;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace BasicWinformm
 {
-    public partial class frmUser : Form
+    class frmUser
     {
-        Person person;
-        public frmUser(string idPerson = "1")
-        {
-            InitializeComponent();
-            person = Person.Get(idPerson);
-            if (person != null)
-            {
-
-                txbHoten.Text = $"{person.LastName} {person.FirstName}";
-              //  txbHoten.Text = person.FullName;
-                dtpNgaysinh.Value = person.DOB;
-                if (person.Sex == EGioiTinh.Nam)
-                {
-                    rdNam.Checked = true;
-                }
-                else rdNu.Checked = true;
-                txbQuequan.Text = person.HowTown;
-            }
-
-            
-        }
-
-        
     }
 }
